1f13a2e02a0fd043a9f1d55da72dfca5b636d017
Jean Disset <jdisset@mit.edu>
2018-03-19 17:26:05 -0400

    fresh

--- basic/src/mainviewer.cpp
+++
@@ -1,9 +0,0 @@
-#include <mecacell/mecacell.h>
-#include <mecacell/viewer/viewer.h>
-#include "core/scenario.hpp"
-#include "core/cell.h"
-
-int main(int argc, char *argv[]) {
-	MecacellViewer::Viewer<Scenario<Cell>> v(argc, argv);
-	return v.exec();
-}
