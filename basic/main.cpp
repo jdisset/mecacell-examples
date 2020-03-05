ff18697fbfb7e9a7be623cab2c28da5f6c861772
Jean Disset <j.disset@gmail.com>
2020-03-04 19:11:33 -0500

    very simple example

--- basic/main.cpp
+++ basic/main.cpp
@@ -2,6 +2,11 @@
 
 int main(int, char **) {
 	Scenario sc;
-	while (!sc.finished()) sc.loop();
+
+	while (!sc.finished()) {
+		sc.loop();
+		std::cout<< "Frame " << sc.w.getNbUpdates() << " : " << sc.w.cells.size() << " cells"
+		          << std::endl;
+	}
 	return 0;
 }
--- basic/scenario.hpp
+++ basic/scenario.hpp
@@ -1,60 +1,71 @@
 #include <mecacell/mecacell.h>
+#include <mecacell/pbdbody.hpp>
 #include <chrono>
-#include <iostream>
-#include <mecacell/springbody.hpp>
-#include <random>
 
-// We need to define a Cell class. This class must inherit from one of the base MecaCell
-// cells classes (or implement all of their basics methods). In doing so, it must also
-// provide a "body implementation". A cell body is a class that handles the physics of
-// cellular interactions (both cell-cell and cell-world) as well as provide storage and
-// access to physics-related state variable for each cell. For example, in the very
-// simple SpringBody model, such variables are the cell's center position, velocity,
-// orientation, stiffness and radius.
-// MecaCell's base cell classes use the CRTP programing idiom (where the base class is
-// templated with the inheriting one) and require a body implementation class as second
-// template argument (default is SpringBody).
-// Here we use the MecaCell::ConnectableCell base class (which just maintains a list of
-// connected cells) with the default SpringBody.
-class Cell : public MecaCell::ConnectableCell<Cell, MecaCell::SpringBody> {
- public:
-	using parent = MecaCell::ConnectableCell<Cell, MecaCell::SpringBody>;
-	using parent::parent;  // Constructor inheritance
+using body_t = MecaCell::PBDBody_singleParticle;
+
+struct Cell {
+	static const constexpr double BASE_LENGTH = 1.0;
+	static const constexpr double GROWTH_SPEED = 1.0;
+
+	size_t id;  // required by world
+	double divisionProgress = 0;
+
+	void updateSize(body_t& b) {
+		double currentLength = BASE_LENGTH * (1.0 + divisionProgress);
+		b.setLength(currentLength);
+	}
+
+	template <typename W, typename B> void divide(W& w, B& body, size_t threadId) {
+		// we create the daughter cell and its body
+		B b(body.getPosition());  // we place the new body at the same position
+		Cell c(*this);            // we use the default copy constructor
+
+		divisionProgress = 0;  // we reset division progress
+		c.divisionProgress = 0;
+
+		updateSize(body);
+		c.updateSize(b);
+
+		// we need to very slightly nudge the two middle nodes (as they are still confounded)
+		const double NUDGE_MAGNITUDE = 0.0001;
+		auto v = MecaCell::Vec::randomUnit();
+		b.particles[0].position += v * NUDGE_MAGNITUDE;
+
+		body.resetProjections();
+		b.resetProjections();
+
+		w.addCell(std::move(c), std::move(b), threadId);
+	}
 
 	// A MecaCell::World (the environment which contains the cells) require each agent (each
 	// cell) to provide an implementation of updateBehavior. This methods takes a reference
 	// to the world itself (here we already know that the W typename is going to be
 	// MecaCell::World<Cell>) and is, by default, called at every world update for each
 	// cell.
-	template <typename W> void updateBehavior(W&) {
+	template <typename W> void updateBehavior(W& w, body_t& b, size_t threadId) {
 		// we can do whatever we want here... such as making the cell divide
-	}
-
-	// SpringBody (and most other body implementations in MecaCell) requires that the cells
-	// define a getAdhesionWith method that, for a given cell pointer and orientation
-	// vector, returns a real value proportional to the desired adhesion intensity with the
-	// given cell. Usually the final adhesion value between two given cell is defined as the
-	// minimum getAdhesionWith value for the two cells (but this depends on the body
-	// implementation you chose, so you should check)
-	double getAdhesionWith(Cell*, const MecaCell::Vector3D&) {
-		// here we just return 0 (no adhesion, only collisions between cells)
-		return 0;
+		// grow & divide
+		if (divisionProgress < 1) {  // grow
+			divisionProgress += GROWTH_SPEED * w.getDt();
+			updateSize(b);
+		} else {
+			divide(w, b, threadId);
+		}
 	}
 };
 
 // A scenario class is a useful wrapper for our simulation,
 // and a requirement for using MecaCellViewer
-class Scenario {
-	using World = MecaCell::World<Cell>;
+struct Scenario {
+	using World = MecaCell::World<Cell, body_t>;
 	using V = MecaCell::Vector3D;
 
- protected:
-	const double SIM_DURATION = 10.0;
+	const double SIM_DURATION = 12.2;
 	World w;             // Our scenario contains an instance of a MecaCell World
 	double simTime = 0;  // Just to keep track of simulated time
 	std::chrono::time_point<std::chrono::system_clock> start;  // keep track of real time
 
- public:
 	Scenario() {
 		start = std::chrono::system_clock::now();
 
@@ -65,11 +76,9 @@ class Scenario {
 		// each program run. Default seed is 0.
 		MecaCell::Config::globalRand().seed(std::random_device{}());
 
-		// We add two cells at the "center of the world"
-		// The MecaCell::World will take ownership of the given pointer and handle all memory
-		// and object lifetime related questions. A
-		w.addCell(new Cell(V(-30, 0, 0)));
-		w.addCell(new Cell(V(30, 0, 0)));
+		// Adding cells require both a Cell object (the controller and high level methods) and
+		// a body object (the physics implementation and representation of that cell)
+		w.addCell(Cell(), body_t(V(0, 0, 0)));
 
 		// When we add cells to the world (using addCell), they are actually stored in a
 		// buffer, and thus are not immediately accessible in World::cells (the cell
@@ -91,12 +100,10 @@ class Scenario {
 		// but it would be cleaner to use a plugin and/or the updateBehavior of each cell
 		// ...
 		//
-		std::cerr << nlohmann::json(w.getExport()).dump(2) << std::endl;
+		// std::cerr << nlohmann::json(w.getExport()).dump(2) << std::endl;
 
 		w.update();
 	}
-
-	World& getWorld() { return w; }  // required by the viewer
 
 	// Convenience function : returns true when the scenario is finished. (Here we also
 	// print out the time it took)
@@ -105,6 +112,7 @@ class Scenario {
 			std::chrono::duration<double> diff = std::chrono::system_clock::now() - start;
 			std::cout << w.getNbUpdates() << " updates in " << diff.count() << " seconds."
 			          << std::endl;
+			std::cout<<"Simulation reached "<<w.cells.size()<<" cells"<<std::endl;
 			return true;
 		};
 		return false;
